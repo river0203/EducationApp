ea3d360c561631949b8337bccbaac6225160dc6b
river0203 <aspp3666@gmail.com>
2025-06-23 23:06:43 +0900

    FIX : Return menu

--- app/src/main/java/com/example/myapplication/BoatActivity.java
+++ app/src/main/java/com/example/myapplication/BoatActivity.java
@@ -9,6 +9,6 @@ public class BoatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_boat);
     }
 }
